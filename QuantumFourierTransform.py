129214db4e901b23b0b0c53fd1d7d350decf2bb7
Dr_HuWenqing <huwenqing.pku@gmail.com>
2021-08-12 19:17:29 +0800

    Add files via upload

---
+++ QuantumFourierTransform.py
@@ -0,0 +1,12 @@
+# initialization
+import numpy as np
+
+# importing Qiskit
+from qiskit import IBMQ, Aer
+from qiskit.providers.ibmq import least_busy
+from qiskit import QuantumCircuit, assemble, transpile
+
+# import basic plot tools
+from qiskit.visualization import plot_histogram
+import matplotlib.pyplot as plt
+
